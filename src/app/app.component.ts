64aa5c282f14a1705b89df431cdd75c087158571
info-forense <info.forense.cuu@gmail.com>
2021-05-27 12:04:30 -0700

    v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'registros-angular';
+  title = 'Sistema de Registros ';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { DatosComponent } from './components/datos/datos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    DatosComponent
   ],
   imports: [
     BrowserModule
